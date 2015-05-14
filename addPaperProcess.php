16b529be55150ef4bb7a634c47925c8e24045613
Maria93 <marysamuelu@gmail.com>
2015-05-14 17:27:07 +1200

    Updated the paper process page so that it takes input from the form - Jaspreet Walia

--- addPaperProcess.php
+++ addPaperProcess.php
@@ -5,45 +5,73 @@
 
 	
 	require_once('connect.php');
-	
-	//Generates the query to be sent to mysql 
-	$query = "INSERT INTO post(title,author,journal,date,abstract,link,rating,reason,aurate,researchLevel,methodology,practice,benefits,
-                                context, result,integrity,conrating,conreason,conrate,reques,method,metrics,participants) 
-	VALUES ('".$_POST['title']."',
-			'".$_POST['author']."',
-			'".$_POST['journal']."',
-			'".$_POST['date']."',
-			'".$_POST['abstract']."'
-			'".$_POST['link']."',
-			'".$_POST['rating']."',
-			'".$_POST['reason']."',
-			'".$_POST['aurate']."',
-			'".$_POST['researchL']."',
-			'".$_POST['methodology']."',
-			'".$_POST['practise']."',
-			'".$_POST['benefits']."',
-			'".$_POST['context']."',
-			'".$_POST['result']."',
-			'".$_POST['integrity']."',
-			'".$_POST['conrating']."',
-			'".$_POST['conreason']."',
-			'".$_POST['conrate']."',
-			'".$_POST['reques']."',
-			'".$_POST['method']."',
-			'".$_POST['metrics']."',
-			'".$_POST['participants']."',
-			)";
-		
-		
-			
-			
-	//trys to send query to mysql
-	if($conn->query($query) == true){
-		//do if query was successful
-		echo "Entry success";
-	}
-	//do if query wasn't successful
-	else{echo "Error: $conn->error";}
+
+    //checks if connection is established 
+    if(!$conn) {
+        echo "<p>Sorry but we could not submit your paper at this time. PLease try again later
+        <br>
+        <br>
+        <a href='index.html'>Return Home</a> 
+        </p>";
+    }
 	
 
+	//Generates the query to be sent to mysql 
+    else {
+
+    $title = $_POST['title']; 
+    $author = $_POST['author']; 
+    $journal = $_POST['journal'];
+    $year = $_POST['date']; 
+    $abstract = $_POST['abstract'];
+    $link = $_POST['link']; 
+    $rating = $_POST['rating']; 
+    $rateReason = $_POST['reason'];
+	$rater = $_POST['aurate']; 
+	$researchLevel = $_POST['researchL'];
+	$methodology = 	$_POST['methodology'];
+	$practise = $_POST['practise'];
+	$evBenefit = $_POST['benefits'];
+	$evContext = $_POST['context'];
+	$evResult = $_POST['result'];
+	$integrity = $_POST['integrity']; 
+	$conRate = $_POST['conrating'];
+	$evRateReason = $_POST['conreason'];
+	$evRater =	$_POST['conrate'];
+	$reQuestion = $_POST['reques'];
+	$reMethod = $_POST['method'];
+	$reMetrics = $_POST['metrics'];
+	$participant = 	$_POST['participants'];
+
+    //Sql command
+	$query = "INSERT INTO $table". "(title,author,journal,dateYear,abstract,link,rating,rateReason,rater,researchLevel,methodology,practice,evBenefit,
+                                evContext, evResult,integrity,conRate,evRateReason,evRater,reQuestion,reMethod,reMetrics,participant)". 
+	"VALUES"."('$tite', '$author', '$journal', '$year', '$abstract', '$link', '$rating', '$rateReason', '$rater', '$researchLevel', '$methodology',
+			'$practise', '$evBenefit', '$evContext', '$evResult', '$integrity', '$conRate', '$evRateReason', '$evRater', '$reQuestion', '$reMethod',
+            '$reMetrics', '$participant')";
+
+    //executes the query
+    $result = mysqli_query($conn, $query); 
+		
+		
+	//checks if exectuion was sucessful 
+    if(!$result) {
+        echo "<p>Something is wrong with ", $query, "
+        <br>
+        <br>
+        <a href = 'index.html'>Return to Home</a>
+        </p>";
+    }
+    else {
+        echo "<p> Your Paper will be considered for submission. Thank You
+        <br>
+        <br>
+        <a href='index.html.Return to Home</a>
+        </p>";
+    }
+    mysqli_close($conn);
+ }
+ 		
+			
+	
 ?>
---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = "cmslamp14.aut.ac.nz";
+$username = "wbv5134";
+$password= "Siatua12"; 
+$db = "wbv5134";
+
+$table = "SERLERpaperinfo";
+
+//Create connection 
+$conn = new mysqli($host, $username, $password, $db); 
+
+
+?>
+
